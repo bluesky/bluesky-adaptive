54c6535351d933a1b34e6e17e1c817b4bcc9afa6
Thomas A Caswell <tcaswell@bnl.gov>
2021-03-09 13:30:01 -0500

    MNT: tweak docstring, remove redundant plan, extra imports

--- bluesky_adaptive/on_stop.py
+++ bluesky_adaptive/on_stop.py
@@ -1,5 +1,5 @@
 """
-Per-Start adaptive integration.
+Per-Start adaptive integration, but triggered on the stop document.
 
 These functions are for integrating adaptive logic that works
 between runs.  The decision making process can expect to consume a
@@ -12,13 +12,7 @@ dictates.
 This corresponds to a "middle" scale of adaptive integration into
 data collection.
 """
-import uuid
-import itertools
-from queue import Queue, Empty
-
-import bluesky.preprocessors as bpp
-import bluesky.plan_stubs as bps
-import bluesky.plans as bp
+from queue import Queue
 
 from .recommendations import NoRecommendation
 
@@ -168,96 +162,3 @@ def recommender_factory(
         run.events.completed.connect(tell_recommender)
 
     return stream_documents_into_runs(tell_recommender_on_completion), queue
-
-
-def adaptive_plan(
-    dets,
-    first_point,
-    *,
-    to_recommender,
-    from_recommender,
-    md=None,
-    take_reading=bp.count
-):
-    """
-    Execute an adaptive scan using an inter-run recommendation engine.
-
-    Parameters
-    ----------
-    dets : List[OphydObj]
-       The detector to read at each point.  The dependent keys that the
-       recommendation engine is looking for must be provided by these
-       devices.
-
-    first_point : Dict[Settable, Any]
-       The first point of the scan.  The motors that will be scanned
-       are extracted from the keys.  The independent keys that the
-       recommendation engine is looking for / returning must be provided
-       by these devices.
-
-    to_recommender : Callable[document_name: str, document: dict]
-       This is the callback that will be registered to the RunEngine.
-
-       The expected contract is for each event it will place either a
-       dict mapping independent variable to recommended value or None.
-
-       This plan will either move to the new position and take data
-       if the value is a dict or end the run if `None`
-
-    from_recommender : Queue
-       The consumer side of the Queue that the recommendation engine is
-       putting the recommendations onto.
-
-    md : dict[str, Any], optional
-       Any extra meta-data to put in the Start document
-
-    take_reading : plan, optional
-        function to do the actual acquisition ::
-
-           def take_reading(dets, md={}):
-                yield from ...
-
-        Callable[List[OphydObj], Optional[Dict[str, Any]]] -> Generator[Msg], optional
-
-        This plan must generate exactly 1 Run
-
-        Defaults to `bluesky.plans.count`
-
-    """
-    # extract the motors
-    motors = list(first_point.keys())
-    # convert the first_point variable to from we will be getting
-    # from queue
-    first_point = {m.name: v for m, v in first_point.items()}
-
-    _md = {"batch_id": str(uuid.uuid4())}
-
-    _md.update(md or {})
-
-    @bpp.subs_decorator(to_recommender)
-    def gp_inner_plan():
-        # drain the queue in case there is anything left over from a previous
-        # run
-        while True:
-            try:
-                from_recommender.get(block=False)
-            except Empty:
-                break
-        uids = []
-        next_point = first_point
-        for j in itertools.count():
-            # this assumes that m.name == the key in Event
-            target = {m: next_point[m.name] for m in motors}
-            motor_position_pairs = itertools.chain(*target.items())
-
-            yield from bps.mov(*motor_position_pairs)
-            uid = yield from take_reading(dets + motors, md={**_md, "batch_count": j})
-            uids.append(uid)
-
-            next_point = from_recommender.get(timeout=1)
-            if next_point is None:
-                return
-
-        return uids
-
-    return (yield from gp_inner_plan())
