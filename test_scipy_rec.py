1e24ebe597b400a49e8dbc27076f27c38a3de71d
Thomas A Caswell <tcaswell@bnl.gov>
2021-03-10 10:25:29 -0500

    TST: add test for bluesky-live wrapper and minimize reccomneder

---
+++ test_scipy_rec.py
@@ -0,0 +1,30 @@
+import pytest
+import bluesky.plan_stubs as bps
+
+
+from bluesky_adaptive.per_start import adaptive_plan
+from bluesky_adaptive.on_stop import recommender_factory
+
+
+def test_seq_recommender(RE, hw):
+    pytest.importorskip("scipy")
+    from bluesky_adaptive.scipy_reccomendations import MinimizerReccomender
+
+    results_list = []
+
+    def do_the_thing(det, det_key):
+        recommender = MinimizerReccomender(scale=-1)
+        cb, queue = recommender_factory(
+            recommender, ["motor"], [det_key], max_count=100
+        )
+        yield from adaptive_plan(
+            [det], {hw.motor: 1}, to_recommender=cb, from_recommender=queue
+        )
+        yield from bps.mv(hw.motor, recommender.result.x)
+        print(recommender.result)
+        results_list.append(recommender.result)
+
+    RE(do_the_thing(hw.det, "det"), print)
+    RE(do_the_thing(hw.img, "img"), print)
+    assert len(results_list) == 2
+    assert all(_ is not None for _ in results_list)
