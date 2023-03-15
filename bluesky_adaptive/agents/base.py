e0de85fd9144ee66353e70a1dad74f9fa6a792e0
maffettone <phillip.maffettone@gmail.com>
2023-03-15 17:36:16 -0400

    ENH: Add batch size as default key

--- bluesky_adaptive/agents/base.py
+++ bluesky_adaptive/agents/base.py
@@ -574,6 +574,7 @@ class Agent(ABC):
         for batch_idx, next_point in enumerate(next_points):
             doc["suggestion"] = next_point
             doc["batch_idx"] = batch_idx
+            doc["batch_size"] = len(next_points)
             self._write_event("ask", doc, uid=uid)
         logger.info(f"Issued ask and adding to the queue. {uid}")
         self._add_to_queue(next_points, uid)
